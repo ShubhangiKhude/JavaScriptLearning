ddbfb58079c9b0823001fff57d368adcd9268c6d
ShubhangiKhude <shubhangikhude3@gmail.com>
2022-12-08 10:08:18 +0530

    Declaration and Initiallization variable in JS
    Declaration and Initiallization variable in JS

---
+++ 01_variable.js
@@ -0,0 +1,12 @@
+var myName;   //Variable Declaration
+myName= "Shubha"; // Variable initiallization
+console.log(myName);
+
+var myFrName= "Shail Mane";
+console.log(myFrName);
+
+var myFrName= "Nil Deshmukh";
+console.log(myFrName);
+
+var [firstName, lastName, age]=["Shubhangi","Khude","23"];
+console.log(firstName, lastName,age);
---
+++ 01_variable_assign.js
@@ -0,0 +1,16 @@
+var myFullName;
+console.log(myFullName);  //Declared without initiallization
+
+var myFullName="Shubhangi Khude";        //Declared and initiallization on same line
+console.log(myFullName);          
+
+var myFullName="Shubhangi Sunil Khude";        //Update
+console.log(myFullName);
+
+var myLove= "I love only JS";
+console.log(myLove);
+
+//Declared and initiallization more than two variables on same line...
+var [firstName, lastName, age, address, pin_code, gender, is_married]=["Shubhangi","Khude", "24","Satara","415015", "Female","No"];
+console.log(firstName,lastName,age,address,pin_code,gender,is_married);
+
