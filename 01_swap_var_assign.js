35539be0bf56e62e07c1d7c82aa0916f9710eed5
ShubhangiKhude <shubhangikhude3@gmail.com>
2022-12-09 13:04:53 +0530

    Swapping values..
    Swapping values..

---
+++ 01_swap_var_assign.js
@@ -0,0 +1,24 @@
+console.log("Swap two values using third variable...");
+console.log("------Before Swaping------");
+var name1= "Sweety";
+var name2= "Cutie";
+console.log(name1,name2);
+console.log("------After Swaping-----");
+var name3= name1;
+name1= name2;
+name2= name3;
+console.log(name1,name2);
+console.log("===================================================");
+
+console.log("Swap three variable values..");
+console.log("-----Before Swaping-----");
+var num1= 100;
+var num2= 200;
+var num3= 300;
+console.log("num1= 100, num2= 200, num3= 300");
+console.log("------After Swaping------");
+var num4= num1;
+num1= num2;
+num2= num3;
+num3= num4;
+console.log(`num1= ${num1}, num2= ${num2}, num3= ${num3}`);
