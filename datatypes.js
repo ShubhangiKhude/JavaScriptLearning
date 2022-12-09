2e3e0f17d11e15f4fa3b42e191cf40a057d558fc
ShubhangiKhude <shubhangikhude3@gmail.com>
2022-12-09 13:04:24 +0530

    Create file learning Values & it's Datatype.
    Create file learning Values & it's Datatype.

---
+++ datatypes.js
@@ -0,0 +1,8 @@
+var PIN= 415015;
+console.log(`PIN: ${PIN}, Type: ${typeof(PIN)}`);
+
+var city= "Satara";
+console.log(`city: ${city}, Type: ${typeof(city)}`);
+
+var is_married= false;
+console.log(`is_married: ${is_married}, Type: ${typeof(is_married)}`);
