c2411bdf372eeb3d4b83f0f0ccfd2f79dfede3c8
ShubhangiKhude <shubhangikhude3@gmail.com>
2022-12-10 10:10:53 +0530

    Create one file practice on Dtatypes..
    Create one file practice on Dtatypes..

---
+++ 01_typeOf.js
@@ -0,0 +1,9 @@
+var bankName;
+console.log(bankName);
+
+bankName= "SBI Bank";
+console.log(`bankName: ${bankName}, type: ${typeof(bankName)}`);
+console.log("\n");
+
+var marks= "90%";
+console.log(`marks: ${marks}`);
