858831361b7467974f37083d0b75bf64f5b73611
ShubhangiKhude <shubhangikhude3@gmail.com>
2022-12-14 20:42:59 +0530

    Update datatypes file
    Update datatypes file

--- datatypes.js
+++ datatypes.js
@@ -8,15 +8,3 @@ var is_married= false;
 console.log(`is_married: ${is_married}, Type: ${typeof(is_married)}`);
 
 
-console.log("======== String template ==========");
-console.log(`Yes you can do it "Sanket"  `);
-
-
-var firstName = "Mohit ";
-var lastName = "Sharma";
-console.log("First name is: "+ firstName + "Last name is: "+ lastName );
-
-console.log(`First name is: ${firstName} and last name is ${lastName}`);
-
-
-console.log("Hello", "\nGood", " \nMorning"  );
