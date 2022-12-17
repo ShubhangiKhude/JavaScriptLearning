0346c4e4d0ba7f3b81188d4b0da7c564b54ec73b
ShubhangiKhude <shubhangikhude3@gmail.com>
2022-12-17 23:16:35 +0530

    Checking comparison result.
    Checking comparison result.

---
+++ 04_checkResult.js
@@ -0,0 +1,26 @@
+var n1= 0 ==' ';
+console.log(`Result of [0 == ''] is :--> ${n1}`);
+var n2= 0 =='0';
+console.log(`Result of [0 == '0'] is :--> ${n2}`);
+var n3= 0 == false;
+console.log(`Result of [0 == false] is :--> ${n3}`);
+var n4= null == undefined;
+console.log(`Result of [null == undefined] is :--> ${n4}`);
+var n5= 1 == [1];
+console.log(`Result of [1 == [1]] is :--> ${n5}`);
+var n6= 1 == true;
+console.log(`Result of [1 == true] is :--> ${n6}`);
+var n7= 1 == '1';
+console.log(`Result of [1 == '1'] is :--> ${n7}`);
+
+
+
+
+
+
+
+
+
+
+
+
