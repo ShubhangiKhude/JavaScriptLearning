47d3f4c54d94e74ef9fb4f70869adc851ba07065
ShubhangiKhude <shubhangikhude3@gmail.com>
2023-01-02 16:26:04 +0530

    Prime number...
    Prime number...

---
+++ primeNumber.js
@@ -0,0 +1,13 @@
+let number = 11;
+var isPrime = "true";
+for (let i = 2; i < number; i++) {
+    if (number % i == 0) {
+        isPrime = false;
+        break;
+    }
+}
+if (isPrime) {
+    console.log(`${number} is a prime number`);
+} else {
+    console.log(`${number} is a not prime number`);
+}
