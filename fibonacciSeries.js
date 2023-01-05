b8ddcce904cfaff0becbdb214ff7ca22ac400e42
ShubhangiKhude <shubhangikhude3@gmail.com>
2023-01-05 23:51:11 +0530

    Update Fibonacci Series..
    Update Fibonacci Series..

--- fibonacciSeries.js
+++ fibonacciSeries.js
@@ -1,7 +1,7 @@
 console.log(`----------Fibonacci Series-----------`);
 console.log(`Fibonacci Series up to 7...`);
 
-let n = 7,n1= 0, n2 = 1, nextTerms;
+let n = 10,n1= 0, n2 = 1, nextTerms;
 for (let index = 0; index < n; index++) {
     console.log(n1);
     nextTerms = n1 + n2;
