b62e74a6c22e5f4df61cf1a9d76d979b1b74cbda
ShubhangiKhude <shubhangikhude3@gmail.com>
2023-01-06 08:57:24 +0530

    Learning Hoisting , Closure, HigherOrder Function and First Class Citizen
    6/1/23
    Start to Learning Hoisting , Closure, HigherOrder Function and First Class Citizen in JS

---
+++ 10_hoisting.js
@@ -0,0 +1,21 @@
+console.log(city);
+
+var city = "Pune";
+
+
+let state = "MH";
+console.log(state);
+
+show();             // normal function in hoisted...we can accessed before declaration..
+
+function show(){
+    console.log("Hello I am in show");
+}
+
+// display(); // function never hoisted..   
+
+var display = function(){
+    console.log("Hello I am in display");
+}
+display(); 
+
